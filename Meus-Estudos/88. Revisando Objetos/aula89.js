1e2e435c2a9572d830202f1dceea6020da5b8e2c
rawRocha <devrochajr@gmail.com>
2024-04-03 20:21:14 -0300

    Update aula89.js

--- Meus-Estudos/88. Revisando Objetos/aula89.js
+++ Meus-Estudos/88. Revisando Objetos/aula89.js
@@ -26,4 +26,71 @@ pessoa1.falaNome = function(){
 
 pessoa1.falaNome()
 
+//adicionando idade a pessoa1
+pessoa1.idade = 04
 
+//adicionando metodo getAnoDeNascimento
+pessoa1.getAnoDeNascimento = function(){
+    const now = new Date()
+    return now.getFullYear() - this.idade
+}
+
+console.log(`${pessoa1.nome} tem ${pessoa1.idade} anos, nasceu em 
+${pessoa1.getAnoDeNascimento()}.`)
+
+//iterando sobre chaves de pessoa1
+for (let chave in pessoa1){
+    console.log(chave)
+}
+
+//iterando sobre valores de pessoa1
+for (let valor in pessoa1){
+    console.log(pessoa1[valor])
+}
+
+//Factory function (padrão de projeto)
+/*
+Função Fábrica:
+Uma função fábrica é essencialmente um gerador de objetos. Em JavaScript, qualquer função pode retornar um objeto.
+Quando uma função não é uma classe ou um construtor, ela é considerada uma função fábrica.
+Em outras palavras, a função fábrica cria e retorna objetos sem a necessidade do operador new.
+*/
+const criaPessoa = function(nome, sobrenome, idade){
+    return{
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade,
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`
+        }
+    }
+}
+
+const pessoaFactory = criaPessoa('Lucas Gabriel', 'Marcolino', 16)
+console.log(pessoaFactory)
+console.log(pessoaFactory.nomeCompleto)
+
+//Constructor function (padrão de projeto)
+
+/* Função Construtora:
+Uma função construtora é uma função que pode ser executada diretamente ou utilizada para instanciar um objeto usando a palavra reservada new.
+Quando usamos o new, o this dentro da função se refere ao objeto criado.
+Funções construtoras são frequentemente usadas para criar múltiplas instâncias de objetos no mesmo contexto. */
+
+function Pessoa(nome, sobrenome){
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.nomeCompleto = function(){
+        return `${this.nome} ${this.sobrenome}`
+    }
+}
+
+const pessoaConstruida = new Pessoa('Fulano', 'De Tal')
+console.log(pessoaConstruida)
+
+pessoaConstruida.nome = 'Outra Coisa'
+
+//obs: pessoaConstruida.nome está alterando o valor da chave no mesmo endereço de memória, pessoaConstruida.ENDERECOMEMORIA = {nome: 'Outra Coisa'}, para evitar a alteração do valor da chave podemos travar o this,
+//Object.freeze(this)
+
+console.log(pessoaConstruida.nomeCompleto())
